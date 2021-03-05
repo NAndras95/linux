bd95e37f8f6373b27c2fea42ff6e44ac523762af
Andras Nagy <andras.nagy@shiwaforce.com>
2021-03-05 08:43:14 +0100

    first commit

---
+++ 00-aliases.sh
@@ -0,0 +1,7 @@
+alias edithosts='sudo nano /etc/hosts'
+alias dokcer='docker'
+alias k='kubectl'
+alias wk='watch kubectl'
+alias kx='kubectx'
+alias kgpo='kubectl get pod'
+alias dfimage="docker run -v /var/run/docker.sock:/var/run/docker.sock --rm alpine/dfimage"
---
+++ README.md
@@ -0,0 +1,6 @@
+# Linux materials
+
+### Contents
+
+**Aliases**
+My aliases collecet in `00-aliases.sh` file.
